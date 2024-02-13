799cc9840dfc04a3f2e5f85d6431961a2a2220d4
Logan Davis <overhome2@gmail.com>
2024-02-13 12:00:53 -0500

    Created GPU functions for inversions; code cleanup

--- src/Invertrix.py
+++ src/Invertrix.py
@@ -14,45 +14,69 @@ Last Modified: 2/12/2024
 import os
 import cv2
 import numpy as np
+from numba import jit
 
-def main():
-    # Create list of image files to be read in and processed
-    img_dir = os.fsencode("./data/input")
+@jit(target_backend='cuda')
+def verticalInvert(img, height, width):
+    new_image = np.zeros((height, width, 3))
+    for y in range(0, height):
+        for x in range(0, width):
+            new_image[y, x] = img[height-y-1, x]
+    
+    return new_image
+
+@jit(target_backend='cuda')
+def horizontalInvert(img, height, width):
+    new_image = np.zeros((height, width, 3))
+    for y in range(0, height):
+        for x in range(0, width):
+            new_image[y, x] = img[y, width-x-1]
+    
+    return new_image
+
+@jit(target_backend='cuda')
+def completeInvert(img, height, width):
+    new_image = np.zeros((height, width, 3))
+    for y in range(0, height):
+        for x in range(0, width):
+            new_image[y, x] = img[height-y-1, width-x-1]
+    
+    return new_image
+
+def detectInputFiles(dirPath):
     img_files = []
+
+    img_dir = os.fsencode(dirPath)
     for file in os.listdir(img_dir):
         img_filename = os.fsdecode(file)
         if img_filename.endswith(".png"):
             img_files.append(img_filename)
-            print(img_filename)
+
+    return img_files
+
+def main():
+    img_files = detectInputFiles("./data/input")
     
-    print("Hello!")
+    print("Starting inversion process on GPU...")
     
     for img_filename in img_files:
         image = cv2.imread("./data/input/" + img_filename, cv2.IMREAD_COLOR)
         height = image.shape[0]
         width = image.shape[1]
 
-        # Vertical invert
-        new_image = np.zeros((height, width, 3))
-        for y in range(0, height):
-            for x in range(0, width):
-                new_image[y, x] = image[height-y-1, x]
+        # Vertical Invert
+        new_image = verticalInvert(image, height, width)
         cv2.imwrite("./data/output/vertical/vertical_" + img_filename, new_image)
 
-        # Horizontal invert
-        new_image = np.zeros((height, width, 3))
-        for y in range(0, height):
-            for x in range(0, width):
-                new_image[y, x] = image[y, width-x-1]
+        # Horizontal Invert
+        new_image = horizontalInvert(image, height, width)
         cv2.imwrite("./data/output/horizontal/horizontal_" + img_filename, new_image)
 
-        # Vertical and Horizontal invert
-        new_image = np.zeros((height, width, 3))
-        for y in range(0, height):
-            for x in range(0, width):
-                new_image[y, x] = image[height-y-1, width-x-1]
+        # Complete Invert
+        new_image = completeInvert(image, height, width)
         cv2.imwrite("./data/output/complete/complete_" + img_filename, new_image)
 
+    print("Done.")
 
 if __name__ == "__main__":
     main()
