79893ac30869d72a6c9c3556fc8b7e448e301a33
Logan Davis <overhome2@gmail.com>
2024-02-13 10:11:11 -0500

    Added loops for creating new images; filled out run.sh file

--- src/Invertrix.py
+++ src/Invertrix.py
@@ -13,6 +13,7 @@ Last Modified: 2/12/2024
 
 import os
 import cv2
+import numpy as np
 
 img_files = []
 
@@ -24,14 +25,36 @@ def main():
         if img_filename.endswith(".png"):
             img_files.append(img_filename)
     
-    for img_file in img_files:
-        img = cv2.imread(img_file, cv2.IMREAD_COLOR)
+    print("Hello!")
+    
+    for img_filename in img_files:
+        image = cv2.imread(img_filename, cv2.IMREAD_COLOR)
+        height = image.shape[0]
+        width = image.shape[1]
 
-        rows = img.shape[0]
-        cols = img.shape[1]
-        for row in range(0, rows):
-            for col in range(0, cols):
-                img[row, col] = 255 # TODO: Change this to swap start and end pixels
+        split_filename = os.path.split(img_filename)
+
+        # Vertical invert
+        new_image = np.zeros((height, width, 3))
+        for y in range(0, height):
+            for x in range(0, width):
+                new_image[y, x] = image[255 - y, x]
+        cv2.imwrite("./data/output/vertical/vertical_" + split_filename, new_image)
+
+        # Horizontal invert
+        new_image = np.zeros((height, width, 3))
+        for y in range(0, height):
+            for x in range(0, width):
+                new_image[y, x] = image[y, 255 - x]
+        cv2.imwrite("./data/output/horizontal/horizontal_" + split_filename, new_image)
+
+        # Vertical and Horizontal invert
+        new_image = np.zeros((height, width, 3))
+        for y in range(0, height):
+            for x in range(0, width):
+                new_image[y, x] = image[255 - y, 255 - x]
+        cv2.imwrite("./data/output/complete/complete_" + split_filename, new_image)
+
 
 if __name__ == "__main__":
     main()
